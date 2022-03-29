560d48cdc287becc97fdb81b54f6d703937c462d
guicastrofer <42876310+guicastrofer@users.noreply.github.com>
2022-03-29 19:38:49 -0300

    initial commit

---
+++ src/main/java/com/zemoga/zemogaportfolioapp/ZemogaPortfolioAppApplication.java
@@ -0,0 +1,13 @@
+package com.zemoga.zemogaportfolioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZemogaPortfolioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZemogaPortfolioAppApplication.class, args);
+	}
+
+}
